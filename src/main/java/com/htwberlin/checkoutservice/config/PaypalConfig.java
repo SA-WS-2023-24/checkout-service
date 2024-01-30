ab41c4977ed1c7fa2cc09da92564df02bb4d28d4
Kristiyan <kristiyan.georgiev9@gmail.com>
2024-01-30 18:59:27 +0100

    paypal login and order works

--- src/main/java/com/htwberlin/checkoutservice/config/PaypalConfig.java
+++ src/main/java/com/htwberlin/checkoutservice/config/PaypalConfig.java
@@ -11,7 +11,7 @@ public class PaypalConfig {
     @Bean
     public PayPalHttpClient getPaypalClient(
             @Value("${paypal.clientId}") String clientId,
-            @Value("${paypal.clientSecret}") String clientSecret) {
+            @Value("${paypal.secret}") String clientSecret) {
         return new PayPalHttpClient(new PayPalEnvironment.Sandbox(clientId, clientSecret));
     }
 }
